e418521e52d92e7742bd5f23a9988059984df0e0
SATHYA <sathyaprakashrai2002@gmail.com>
2022-06-24 14:22:13 +0530

    added buttons for player UI

--- app/screens/player.js
+++ app/screens/player.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import { FontAwesome5, Ionicons } from '@expo/vector-icons';
+import { FontAwesome5, Ionicons, AntDesign, Entypo } from '@expo/vector-icons';
 import reactDom from 'react-dom';
 import Slider from '@react-native-community/slider';
 import color from '../misc/color';
@@ -34,12 +34,19 @@ const Player = () => {
           <Text></Text>
           <View style={styles.sliderbar}>
             <Slider
-              style={{ width: width, height: 40 }}
+              style={{ width: width, height: 20 }}
               minimumValue={0}
               maximumValue={1}
               minimumTrackTintColor='#00D1FF'
               maximumTrackTintColor='#000000'>
             </Slider>
+          </View>
+          <View style={styles.buttons}>
+            <Ionicons name="play-back" size={46} color="black" />
+            <Ionicons name="play" size={46} color="black" />
+            <Ionicons name="play-forward" size={46} color="black" />
+
+
           </View>
         </View>
       </View>
@@ -79,6 +86,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingLeft: 20,
     fontSize: 20,
+    fontWeight: 'bold',
     color: '#000000',
   },
   thewhole: {
@@ -97,6 +105,30 @@ const styles = StyleSheet.create({
     width: width,
     //alignItems: 'center',
     //justifyContent: 'center',
+  },
+  buttons: {
+    //backgroundColor: 'green',
+    marginLeft: 19,
+    marginRight: 19,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    //paddingRight: 35,
+  },
+  backbutt: {
+    alignItems: 'flex-start',
+    paddingLeft: 10,
+    backgroundColor: 'blue',
+  },
+  playbutt: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: 'yellow',
+  },
+  forbutt: {
+    alignItems: 'flex-end',
+    paddingRight: 10,
+    backgroundColor: 'red',
   },
 });
 export default Player;
